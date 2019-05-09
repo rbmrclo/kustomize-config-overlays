7608b5674b334a21fa664e109b489a6956271e62
Robbie Marcelo <rbmrclo@hotmail.com>
2019-05-09 19:04:20 +0800

    Current config

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -eu
+
+# KustomizeVersion:1.0.8
+
+APP_NAME="nginx-deployment"
+
+kubectl config use-context docker-for-desktop
+
+echo
+echo "============================="
+kustomize build base | kubectl apply -f -
+echo "============================="
+
+POD_INFO_1=$(kubectl get pods -l "app=${APP_NAME}" -o json)
+POD_NAME_1="$(echo ${POD_INFO_1} | jq -r '.items[0].metadata.name')"
+
+kubectl rollout status deployment/nginx-deployment
+
+echo "#############################"
+echo "FOO_BAR value:"
+kubectl exec $POD_NAME_1 -it -- printenv FOO_BAR
+echo "#############################"
+
+echo
+echo "============================="
+kustomize build apps/overlays/staging | kubectl apply -f -
+echo "============================="
+echo
+
+# To trigger deployment rollout without deployment.yaml changes
+kubectl rollout status deployment/nginx-deployment
+
+POD_INFO_2=$(kubectl get pods -l "app=${APP_NAME}" -o json)
+POD_NAME_2="$(echo ${POD_INFO_2} | jq -r '.items[0].metadata.name')"
+
+echo "#############################"
+echo "FOO_BAR value:"
+kubectl exec $POD_NAME_2 -it -- printenv FOO_BAR
+echo "#############################"
